e69c9822ced8fc770322092db687c776b31b4cd9
sudharson <sudharson@tuta.io>
2025-11-05 16:35:58 +0530

    Dispatch corrected

--- src/Services/dispatchtherequest.java
+++ src/Services/dispatchtherequest.java
@@ -8,16 +8,17 @@ public class dispatchtherequest {
 	public void dispatch(List<String> validBlood,HashMap<String,Integer> blood,HashMap<String,patientRequest> request_M) {
 		for(String id:request_M.keySet()) {
 			if(blood.containsKey(request_M.get(id).bloodtpy)) {
+				String name=request_M.get(id).name_p;
 				if(request_M.get(id).quantity<=blood.get(request_M.get(id).bloodtpy)) {
 					int a;
+					System.out.println("Blood successfully dispatched for "+name);
 					a=blood.get(request_M.get(id).bloodtpy);
 					a-=(int)(request_M.get(id).quantity);
 					blood.put(request_M.get(id).bloodtpy, a);
 					request_M.remove(id);
-					System.out.println("Blood successfully dispatched");
 				}
 				else {
-					System.out.println("Not enough blood");
+					System.out.println("Not enough blood for "+name);
 				}
 			}
 			else {
